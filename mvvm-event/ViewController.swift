73bd8caa0f02b0870945d10c61568132056c6565
Guilherme Piccoli <guuipiccoli@gmail.com>
2019-04-12 19:02:50 -0300

    test

---
+++ mvvm-event/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-event
+//
+//  Created by Guilherme Piccoli on 12/04/19.
+//  Copyright © 2019 Guilherme Piccoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
